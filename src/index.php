9bd7ed2e3610c00677e92a67129969309f5bfb5b
HotBurgi <mtorri2007@gmail.com>
2026-02-27 11:15:54 +0100

    updated index.php

--- src/index.php
+++ src/index.php
@@ -9,9 +9,9 @@
     <h1>Query Albergo</h1>
     <p>Seleziona una query per visualizzare il risultato:</p>
     <ul>
-        <li><a href="form_query_A.php">Query parametrica A - Prenotazioni passate, dato il nominativo</a></li>
-        <li><a href="query_B.php">Query statica B - Elenco stanze libere nella giornata odierna</a></li>
-        <li><a href="query_C.php">Query statica C - Stanza con piu prenotazioni</a></li>
+        <li><a href="form_query_A.php">Query parametrica A</a></li>
+        <li><a href="form_query_C.php">Query parametrica C</a></li>
+        <li><a href="query_B.php">Query B</a></li>
     </ul>
 </body>
 
